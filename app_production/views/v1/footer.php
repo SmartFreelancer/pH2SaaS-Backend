7023ef0b8afd0c6a21d6615c028eb33c3442b185
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-02-04 12:01:01 +0000

    Remove unused google analytics tracking

--- app_production/views/v1/footer.php
+++ app_production/views/v1/footer.php
@@ -34,30 +34,16 @@
 <script type="text/javascript" src="<?= base_url('v1_assets/plugins/formvalidation/js/framework/bootstrap.min.js'); ?>"></script>
 <script type="text/javascript" src="<?= base_url('v1_assets/js/coreapp.js'); ?>"></script>
 <script src="<?= base_url('v1_assets/js/main.js'); ?>"></script>
-
-<script type="text/javascript">
+<script>
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/55ce7e9d2d7d7e5465512881/default';
+s1.src='https://embed.tawk.to/5895c00764544b46b6b575a5/default';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
 </script>
-
-
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-54493882-5', 'auto');
-  ga('send', 'pageview');
-
-</script>
-
 </body>
 </html>
