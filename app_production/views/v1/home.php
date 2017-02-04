138340ac32c92f22fefde5f5598f702847c13996
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-02-04 08:12:41 +0000

    Update splash homepage for AppNinja Website

--- app_production/views/v1/home.php
+++ app_production/views/v1/home.php
@@ -4,8 +4,7 @@
             <div class="col-md-6 col-md-offset-6">
                 <div class="product-hero">
                     <h1>Tools to Automate the <strong>Marketing</strong> of your <strong>Android Apps</strong></h1>
-                    <p class="italic"><small>(and <strong>Progressive Web Apps</strong> as well!)</small></p>
-                    <p>Be more visible to clients on Fiverr. Improve your search rankings. Earn more from your apps. It’s all possible with our groundbreaking marketing tools for Android publishers.</p>
+                    <p>Improve your search rankings. Earn more from your apps. It’s all possible with our groundbreaking marketing tools for Android publishers.</p>
                     <div class="row">
                         <div class="col-xs-6">
                             <div class="price-tag text-center">
@@ -15,7 +14,7 @@
                                 <span class="sub">.<?= $price_cents; ?></span>
                                 </a>
                                 <a class="active" href="<?= base_url(); ?>payment/membership/2">
-                                <span class="period">Go 3 months, Save 25% ($22.39) →</span>
+                                <span class="period italic">Go 3 months, Save 25% ($22.39) →</span>
                                 </a>
                             </div>
                         </div>
@@ -32,12 +31,11 @@
 </header>
 <section id="services">
 <div class="container">
-    <center>
-    <h2 class="section-heading">The World’s First Platform for Fiverr Sellers</h2>
-    <h3 class="section-subheading text-muted mbn" style="margin-bottom: 0;">Helping you to become more than average.</h3>
-    <br><br><br><br>
-    <img src="<?= base_url(); ?>v1_assets/images/intro.gif" class="img-responsive">
-    </center>
+    <div class="center">
+        <h2 class="section-heading">Effective Automated Marketing for Android Apps</h2>
+        <h3 class="section-subheading text-muted mbn" style="margin-bottom: 0;">Helping your Android App to become more popular and earning more than the average.</h3>
+        <p><img src="<?= base_url(); ?>v1_assets/images/intro.gif" class="img-responsive"></p>
+</div>
 </div>
 </section>
 <section class="pricing-holder gray">
@@ -50,9 +48,9 @@
                     <h3>One Price Full Premium Access.</h3>
                     <p>Sign up with one low cost and get exclusive access to the world’s only freelancing tools designed specifically for Fiverr sellers.</p>
                     <br>
-                    <center>
-                    <a class="btn btn-lg btn-action block" href="<?= base_url(); ?>payment/membership/1">$<?= $default_price; ?> Sign Up Now <span>Payment via Paypal</span></a>
-                    </center>
+                    <div class="center">
+                        <a class="btn btn-lg btn-action block" href="<?= base_url(); ?>payment/membership/1">$<?= $default_price; ?> Sign Up Now <span>Payment via Paypal</span></a>
+                    </div>
                 </div>
                 <div id="pricing-right" class="col-sm-4">
                     <div class="price-tag text-center">
@@ -80,8 +78,8 @@
 <div class="container">
     <div class="row">
         <div class="col-md-6">
-            <h2>Increase your earnings... Fiverr Tools work! </h2>
-            <p>Many Fiverr sellers have used our tools to boost their profits. Sign up today and watch your earnings grow too.</p>
+            <h2>Increase your app's popularity... Marketing Tools works!</h2>
+            <p>Give powerful and organic boosts to your Android app and increase your profits. Sign up today and watch your earnings grow too.</p>
             <br><br>
             <button data-toggle="modal" data-link="<?= base_url(); ?>auth/lead" data-target="#get-started" class="btn btn-primary btn-lg">Increase Your Earnings</button>
         </div>
@@ -97,20 +95,20 @@
             <div class="testi-container">
                 <div class="client-info clearfix">
                     <div class="info pull-left">
-                        <h3 style="" data-selector="h3">Mathew Welby</h3>
+                        <h3 style="" data-selector="h3">John Martinez</h3>
                     </div>
                 </div>
-                <p style="" data-selector="p" class="testi-text">Fiverr Tools is a competitively priced tool with some great functionality. One of the nicest parts of Fiverr Tools is that it’s very simple to use.</p>
+                <p style="" data-selector="p" class="testi-text">I ended up using the marketing campaign on two of my apps, the first one got reviewed on a couple small websites with moderate downloads. The second one got a lot more success with its campaign, it was definitely all worth it.</p>
             </div>
         </div>
         <div class="col-sm-4">
             <div class="testi-container">
                 <div class="client-info clearfix">
                     <div class="info pull-left">
-                        <h3 style="" data-selector="h3">Jake M.</h3>
+                        <h3 style="" data-selector="h3">George Canales</h3>
                     </div>
                 </div>
-                <p style="" data-selector="p" class="testi-text">Fiverr Tools allows us to see what our competitors are doing so we can see what’s working and what’s not within our niche.</p>
+                <p style="" data-selector="p" class="testi-text">After my marketing campaign my app got reviewed on several big sites, including LifeHacker. That obviously resulted in massive downloads, loved the service.</p>
             </div>
         </div>
 
@@ -118,10 +116,10 @@
             <div class="testi-container">
                 <div class="client-info clearfix">
                     <div class="info pull-left">
-                        <h3 style="" data-selector="h3">Katie K. Jacobs</h3>
+                        <h3 style="" data-selector="h3">Asad Azimuddin</h3>
                     </div>
                 </div>
-                <p style="" data-selector="p" class="testi-text">I was really impressed, super easy to use website!</p>
+                <p style="" data-selector="p" class="testi-text">I most enjoyed that the experience was personal, they spent time with my app rather than pushing my app through an impersonal ad network. All of my questions were answered and everything in the campaign was fulfilled as expected.</p>
             </div>
         </div>
     </div>
@@ -130,7 +128,7 @@
 <div class="astyle">
 <a class="button button-full button-default text-center" href="<?= base_url(); ?>features">
 <div class="container clearfix">
-    See how Fiverr Tools can work for you and your freelancing business
+    See how <strong>AppNinja</strong> can work for you and your freelancing business
     <strong>See Features</strong>
     <i class="icon-caret-right" style="top:4px;"></i>
 </div>
