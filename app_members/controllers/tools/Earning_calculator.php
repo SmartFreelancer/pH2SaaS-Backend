8a2b7c03d67ba180a35ad3341552524f87b0d4dc
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-02-05 18:46:38 +0000

    Add earning calculator tool

---
+++ app_members/controllers/tools/Earning_calculator.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Earning_calculator extends MY_Controller {
+
+   public function __construct() {
+      parent::__construct();
+
+      if ($this->data['user']->membership_payment == 0) {
+         redirect('membership-disabled');
+      }
+   }
+
+   public function index() {
+      $this->data['page_title'] = 'Earning Calculator';
+
+      $this->load->view('header', $this->data);
+      $this->load->view('head_nav', $this->data);
+      $this->load->view('tools/earning_calculator/main', $this->data);
+      $this->load->view('footer', $this->data);
+   }
+}
